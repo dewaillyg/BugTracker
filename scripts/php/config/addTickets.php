e618dfed1270b4fc2030af69e3fd7385b405998d
dewaillyg <114297688+dewaillyg@users.noreply.github.com>
2023-05-26 14:25:16 +0200

    add tester form.

--- scripts/php/config/addTickets.php
+++ scripts/php/config/addTickets.php
@@ -1,5 +1,16 @@
 <?php
 
-echo 'ajout de tickets';
+session_start();
+include './connexionBDD.php';
+
+echo date('m/d/Y H:i:s', time());
+
+
+
+// $membersData = $db->query('SELECT * FROM bugtracker_members');
+// $membersData->execute();
+// $members = $membersData->fetchAll();
+
+echo '<br/>'.'ticket validé';
 
 ?>
